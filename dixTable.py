4ad031e39da9ce1573c56e858df313db37e6a922
diogoscf <diogo.scf2003@gmail.com>
2019-12-20 17:43:54 +0100

    Make dixtable scrape from github instead of svn

--- dixTable.py
+++ dixTable.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-import sys, os, argparse, itertools, re, subprocess, urllib.request, logging, collections, textwrap
-import xml.etree.ElementTree as etree
+import sys, os, argparse, itertools, re, subprocess, urllib.request, logging, collections, textwrap, json
 
 iso639Codes = {'roh': 'rm', 'gv': 'glv', 'gu': 'guj', 'ron': 'ro', 'oss': 'os', 'gd': 'gla', 'nld': 'nl', 'ga': 'gle', 'se': 'sme', 'gl': 'glg', 'oji': 'oj', 'oci': 'oc', 'ty': 'tah', 'jav': 'jv', 'tw': 'twi', 'tt': 'tat', 'hrv': 'hr', 'tr': 'tur', 'ts': 'tso', 'tn': 'tsn', 'to': 'ton', 'tl': 'tgl', 'tk': 'tuk', 'th': 'tha', 'ti': 'tir', 'ven': 've', 'tg': 'tgk', 'te': 'tel', 'ta': 'tam', 'fas': 'fa', 'ssw': 'ss', 'de': 'deu', 'da': 'dan', 'ay': 'aym', 'dz': 'dzo', 'fao': 'fo', 'dv': 'div', 'rn': 'run', 'hin': 'hi', 'qu': 'que', 'hye': 'hy', 'guj': 'gu', 'kua': 'kj', 'cre': 'cr', 'div': 'dv', 'bam': 'bm', 'bak': 'ba', 'tel': 'te', 'mi': 'mri', 'za': 'zha', 'mh': 'mah', 'ara': 'ar', 'ce': 'che', 'nbl': 'nr', 'zu': 'zul', 'wa': 'wln', 'sun': 'su', 'abk': 'ab', 'kur': 'ku', 'wol': 'wo', 'lub': 'lu', 'gn': 'grn', 'lug': 'lg', 'jv': 'jav', 'nep': 'ne', 'ms': 'msa', 'iku': 'iu', 'lg': 'lug', 'wo': 'wol', 'tur': 'tr', 'mr': 'mar', 'tuk': 'tk', 'ja': 'jpn', 'cos': 'co', 'ile': 'ie', 'gla': 'gd', 'bos': 'bs', 'gle': 'ga', 'glg': 'gl', 'aka': 'ak', 'bod': 'bo', 'glv': 'gv', 'aa': 'aar', 'ch': 'cha', 'co': 'cos', 'vie': 'vi', 'ipk': 'ik', 'ca': 'cat', 'bs': 'bos', 'por': 'pt', 'uzb': 'uz', 'na': 'nau', 'pol': 'pl', 'cs': 'ces', 'tgk': 'tg', 'bre': 'br', 'cv': 'chv', 'tgl': 'tl', 'aym': 'ay', 'cha': 'ch', 'fra': 'fr', 'che': 'ce', 'pt': 'por', 'swa': 'sw', 'twi': 'tw', 'swe': 'sv', 'pa': 'pan', 'chu': 'cu', 'chv': 'cv', 'vi': 'vie', 'fry': 'fy', 'pi': 'pli', 'msa': 'ms', 'am': 'amh', 'hmo': 'ho', 'iii': 'ii', 'ml': 'mal', 'mg': 'mlg', 'mlg': 'mg', 'ibo': 'ig', 'hat': 'ht', 'slv': 'sl', 'mn': 'mon', 'xho': 'xh', 'deu': 'de', 'mk': 'mkd', 'cat': 'ca', 'mt': 'mlt', 'mlt': 'mt', 'slk': 'sk', 'ful': 'ff', 'my': 'mya', 'tat': 'tt', 've': 'ven', 'jpn': 'ja', 'vol': 'vo', 'oc': 'oci', 'is': 'isl', 'iu': 'iku', 'it': 'ita', 'vo': 'vol', 'ii': 'iii', 'mya': 'my', 'ik': 'ipk', 'io': 'ido', 'spa': 'es', 'ia': 'ina', 'ave': 'ae', 'tah': 'ty', 'ava': 'av', 'ig': 'ibo', 'yo': 'yor', 'eng': 'en', 'ie': 'ile', 'ewe': 'ee', 'id': 'ind', 'nya': 'ny', 'sin': 'si', 'pan': 'pa', 'snd': 'sd', 'mar': 'mr', 'sna': 'sn', 'kir': 'ky', 'kik': 'ki', 'fa': 'fas', 'kin': 'rw', 'ff': 'ful', 'lat': 'la', 'mah': 'mh', 'lav': 'lv', 'mal': 'ml', 'fo': 'fao', 'ss': 'ssw', 'sr': 'srp', 'sq': 'sqi', 'sw': 'swa', 'sv': 'swe', 'su': 'sun', 'st': 'sot', 'sk': 'slk', 'epo': 'eo', 'si': 'sin', 'so': 'som', 'sn': 'sna', 'sm': 'smo', 'sl': 'slv', 'sc': 'srd', 'sa': 'san', 'ido': 'io', 'sg': 'sag', 'nb': 'nob', 'tha': 'th', 'sd': 'snd', 'ita': 'it', 'tsn': 'tn', 'tso': 'ts', 'lb': 'ltz', 'ell': 'el', 'la': 'lat', 'ln': 'lin', 'lo': 'lao', 'li': 'lim', 'lv': 'lav', 'lt': 'lit', 'lu': 'lub', 'fij': 'fj', 'fin': 'fi', 'hau': 'ha', 'eus': 'eu', 'yi': 'yid', 'amh': 'am', 'bih': 'bh', 'dan': 'da', 'nob': 'nb', 'ces': 'cs', 'mon': 'mn', 'bis': 'bi', 'nor': 'no', 'cy': 'cym', 'afr': 'af', 'el': 'ell', 'eo': 'epo', 'en': 'eng', 'ee': 'ewe', 'fr': 'fra', 'lao': 'lo', 'cr': 'cre', 'eu': 'eus', 'et': 'est', 'es': 'spa', 'ru': 'rus', 'est': 'et', 'smo': 'sm', 'cu': 'chu', 'fy': 'fry', 'rm': 'roh', 'sme': 'se', 'ro': 'ron', 'be': 'bel', 'bg': 'bul', 'run': 'rn', 'ba': 'bak', 'ps': 'pus', 'bm': 'bam', 'bn': 'ben', 'bo': 'bod', 'bh': 'bih', 'bi': 'bis', 'orm': 'om', 'que': 'qu', 'br': 'bre', 'ori': 'or', 'rus': 'ru', 'pli': 'pi', 'pus': 'ps', 'om': 'orm', 'oj': 'oji', 'srd': 'sc', 'ltz': 'lb', 'nde': 'nd', 'dzo': 'dz', 'ndo': 'ng', 'srp': 'sr', 'wln': 'wa', 'isl': 'is', 'os': 'oss', 'or': 'ori', 'zul': 'zu', 'xh': 'xho', 'som': 'so', 'sot': 'st', 'fi': 'fin', 'zh': 'zho', 'fj': 'fij', 'yid': 'yi', 'mkd': 'mk', 'kom': 'kv', 'her': 'hz', 'kon': 'kg', 'ukr': 'uk', 'ton': 'to', 'heb': 'he', 'kor': 'ko', 'hz': 'her', 'hy': 'hye', 'hr': 'hrv', 'hun': 'hu', 'ht': 'hat', 'hu': 'hun', 'hi': 'hin', 'ho': 'hmo', 'bul': 'bg', 'ha': 'hau', 'cym': 'cy', 'he': 'heb', 'ben': 'bn', 'bel': 'be', 'uz': 'uzb', 'azb': 'az', 'aze': 'az', 'ur': 'urd', 'zha': 'za', 'pl': 'pol', 'uk': 'ukr', 'aar': 'aa', 'ug': 'uig', 'zho': 'zh', 'nno': 'nn', 'ab': 'abk', 'ae': 'ave', 'san': 'sa', 'uig': 'ug', 'af': 'afr', 'ak': 'aka', 'arg': 'an', 'sag': 'sg', 'an': 'arg', 'as': 'asm', 'ar': 'ara', 'khm': 'km', 'av': 'ava', 'ind': 'id', 'az': 'aze', 'ina': 'ia', 'asm': 'as', 'nl': 'nld', 'nn': 'nno', 'no': 'nor', 'lim': 'li', 'lin': 'ln', 'nd': 'nde', 'ne': 'nep', 'tir': 'ti', 'ng': 'ndo', 'lit': 'lt', 'ny': 'nya', 'nav': 'nv', 'nau': 'na', 'grn': 'gn', 'nr': 'nbl', 'yor': 'yo', 'nv': 'nav', 'kv': 'kom', 'tam': 'ta', 'cor': 'kw', 'kan': 'kn', 'kal': 'kl', 'kas': 'ks', 'sqi': 'sq', 'rw': 'kin', 'kau': 'kr', 'kat': 'ka', 'kaz': 'kk', 'urd': 'ur', 'ka': 'kat', 'kg': 'kon', 'kk': 'kaz', 'kj': 'kua', 'ki': 'kik', 'ko': 'kor', 'kn': 'kan', 'km': 'khm', 'kl': 'kal', 'ks': 'kas', 'kr': 'kau', 'kw': 'cor', 'mri': 'mi', 'ku': 'kur', 'ky': 'kir', 'hbs': 'sh', 'sh': 'hbs'}
 
@@ -121,7 +120,7 @@ def updateOrCreatePages(location, pairName, updateStats, editToken):
 
             pageContents = """
             {{{{TOCD}}}}
-            This is a language pair translating between [[{0}]] and [[{1}]]. The pair is currently located in [https://svn.code.sf.net/p/apertium/svn/{2}/{3} {2}].
+            This is a language pair translating between [[{0}]] and [[{1}]]. The pair is currently located in [https://github.com/apertium/{3} {2}].
 
             == General information ==
             * The {0}-{1} transducer contains {{{{#lst:{4}|{5}-{6}_stems}}}} stems in its bidictionary.""".format(getLangName(lang1), getLangName(lang2), location, pairName, statsPageTitle, stemEntryPair[0], stemEntryPair[1])
@@ -229,40 +228,25 @@ if __name__ == '__main__':
     languages = set(comprehensiveList(args.languages))
     logging.info('Using languages {}'.format(languages))
 
-    dirs = [
-        ('incubator', r'<name>(apertium-(\w{2,3})-(\w{2,3}))</name>'),
-        ('nursery', r'<name>(apertium-(\w{2,3})-(\w{2,3}))</name>'),
-        ('staging', r'<name>(apertium-(\w{2,3})-(\w{2,3}))</name>'),
-        ('trunk', r'<name>(apertium-(\w{2,3})-(\w{2,3}))</name>'),
+    dirPaths = [
+        'incubator',
+        'nursery',
+        'staging',
+        'trunk',
     ]
 
-    for (dirPath, dirRegex) in dirs:
-        svnData = str(subprocess.check_output('svn list --xml https://svn.code.sf.net/p/apertium/svn/%s/' % dirPath, stderr=subprocess.STDOUT, shell=True), 'utf-8')
-        for langDir in re.findall(dirRegex, svnData, re.DOTALL):
-            if set(langDir[1:]) <= languages:
-                primaryPairs[frozenset({langDir[1], langDir[2]})] = (dirPath, langDir[0])
-            elif langDir[1] in languages or langDir[2] in languages:
-                secondaryPairs[frozenset({langDir[1], langDir[2]})] = (dirPath, langDir[0])
-
-    svnData = str(subprocess.check_output('svn list --xml https://svn.code.sf.net/p/apertium/svn/incubator/', stderr=subprocess.STDOUT, shell=True), 'utf-8')
-    for dixFileElem in etree.fromstring(svnData).findall('.//name'):
-        dixFileName = dixFileElem.text
-        if re.match(r'apertium-[^\.]+\.[^\.]+\.dix(?:\.xml)?', dixFileName):
-            try:
-                pair = dixFileName.split('.')[1].split('-')
-                dixLink = 'https://svn.code.sf.net/p/apertium/svn/incubator/%s' % dixFileName
-                if len(pair) > 1 and pair[0] in languages or pair[1] in languages:
-                    if frozenset(pair) not in primaryPairs and frozenset(pair) not in secondaryPairs:
-                        if pair[0] in languages and pair[1] in languages:
-                            primaryPairs[frozenset(pair)] = ('incubator', dixFileName.split('.')[0])
-                        else:
-                            secondaryPairs[frozenset(pair)] = ('incubator', dixFileName.split('.')[0])
-                    else:
-                        logging.error('%s already recorded! Skipping!' % str(pair))
-            except IndexError:
-                pass
-
-    #print(primaryPairs, secondaryPairs)
+    gitData = json.loads(subprocess.check_output('curl -X GET https://apertium.projectjj.com/stats-service/packages/ -H accept: application/json | python -m json.tool', shell=True))["packages"]
+    for package in gitData:
+        langDir = re.findall(r"(apertium-(\w+)-(\w+))", package["name"], re.DOTALL)
+        if langDir:
+            langDir = langDir[0]
+            for topic in package["topics"]:
+                for dirPath in dirPaths:
+                    if re.match(r'apertium-%s' % dirPath, topic):
+                        if set(langDir[1:]) <= languages:
+                            primaryPairs[frozenset({langDir[1], langDir[2]})] = (dirPath, langDir[0])
+                        elif langDir[1] in languages or langDir[2] in languages:
+                            secondaryPairs[frozenset({langDir[1], langDir[2]})] = (dirPath, langDir[0])
 
     pairFormatting = {'trunk': "'''", 'incubator': "''", 'nursery': "", 'staging': "'''''"}
 
